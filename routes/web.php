7379ecf5b71505ceed0262f3b1b624217e5faa57
cfscastillo04 <cfscastillo@outlok.com>
2019-06-19 23:40:05 +0800

    under construction

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,15 @@
 
 
 
-Route::get('/backroom', 'BackroomController@index')->name('backroom');
-Route::get('/', 'HomeController@index')->name('home');
 
-
-
-
-Auth::routes();
-Route::get('/verify-user/{code}', 'Auth\RegisterController@activateUser')->name('activate.user');
 
 
 
 //under construction
 Route::group(['middleware' => 'under-construction'], function () {
+    Route::get('/backroom', 'BackroomController@index')->name('backroom');
+    Route::get('/', 'HomeController@index')->name('home');
 
+    Auth::routes();
+    Route::get('/verify-user/{code}', 'Auth\RegisterController@activateUser')->name('activate.user');
 });
