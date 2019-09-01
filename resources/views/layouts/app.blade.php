3300a79cd33c4dc1cdc2208e90952a5735df47f9
cfscastillo04 <cfscastillo@outlok.com>
2019-09-02 03:54:19 +0800

    redesign

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -275,7 +275,13 @@
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
 </script>
-
+<script type="text/javascript"> //<![CDATA[
+    var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.trust-provider.com/" : "http://www.trustlogo.com/");
+    document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
+    //]]></script>
+<script language="JavaScript" type="text/javascript">
+    TrustLogo("https://www.positivessl.com/images/seals/positivessl_trust_seal_md_167x42.png", "POSEV", "none");
+</script>
 
 </body>
 </html>
