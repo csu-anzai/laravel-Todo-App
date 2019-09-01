9075c9f1240c18583b84cceddae1cc31312d688e
cfscastillo04 <cfscastillo@outlok.com>
2019-09-02 04:41:27 +0800

    redesign

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,13 +35,7 @@
     <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
 </head>
 <body class="bg-white">
-<script type="text/javascript"> //<![CDATA[
-    var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.trust-provider.com/" : "http://www.trustlogo.com/");
-    document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
-    //]]></script>
-<script language="JavaScript" type="text/javascript">
-    TrustLogo("https://www.positivessl.com/images/seals/positivessl_trust_seal_sm_124x32.png", "POSDV", "none");
-</script>
+
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
 <script>
@@ -199,6 +193,15 @@
 
                             @endguest
                             <li class="nav-item px-2"><a href="#" class="btn btn-dark">Get A Quote</a></li>
+                            <li class="nav-item">
+                                <script type="text/javascript"> //<![CDATA[
+                                    var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.trust-provider.com/" : "http://www.trustlogo.com/");
+                                    document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
+                                    //]]></script>
+                                <script language="JavaScript" type="text/javascript">
+                                    TrustLogo("https://www.positivessl.com/images/seals/positivessl_trust_seal_sm_124x32.png", "POSDV", "none");
+                                </script>
+                            </li>
                         </ul>
                     </transition>
                 </div>
